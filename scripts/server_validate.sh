9f10e56ff999dca3cd38083a4cdd87c875a3665a
Derrick Agyemang <derrickagyemang12@outlook.com>
2021-03-18 10:19:39 +0000

    Update server_validate.sh

--- scripts/server_validate.sh
+++ scripts/server_validate.sh
@@ -1,13 +1,13 @@
 
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-while [ true ]
-do
-    if [ "$(curl -s http://localhost:80/health)" = '{"status":"UP"}' ]
-    then
-        exit 0
-    else
-        echo "check server is running?"
-        sleep 3s
-    fi
-done
+# while [ true ]
+# do
+#     if [ "$(curl -s http://localhost:80/health)" = '{"status":"UP"}' ]
+#     then
+#         exit 0
+#     else
+#         echo "check server is running?"
+#         sleep 3s
+#     fi
+# done
