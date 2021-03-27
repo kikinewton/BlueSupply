d3322db6933643583735ed1d4942be1a722ae25c
etornam anyidoho <etornamanyidoho@outlook.com>
2021-03-27 21:18:20 +0000

    junk

--- src/main/java/com/logistics/supply/model/Payment.java
+++ src/main/java/com/logistics/supply/model/Payment.java
@@ -3,8 +3,14 @@ package com.logistics.supply.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
 }
