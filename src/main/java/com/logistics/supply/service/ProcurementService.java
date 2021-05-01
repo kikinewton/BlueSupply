30efa26d958e7b549d1ee0308b58933d6c80d72a
Kiki Newton <derrickagyemang@Derricks-MacBook-Pro.local>
2021-05-01 14:05:40 +0000

    Initialise quotation when multiple suppliers is assigned to request item

--- src/main/java/com/logistics/supply/service/ProcurementService.java
+++ src/main/java/com/logistics/supply/service/ProcurementService.java
@@ -4,6 +4,7 @@ import com.logistics.supply.dto.ProcurementDTO;
 import com.logistics.supply.dto.SetSupplierDTO;
 import com.logistics.supply.enums.EndorsementStatus;
 import com.logistics.supply.enums.RequestStatus;
+import com.logistics.supply.model.Quotation;
 import com.logistics.supply.model.RequestCategory;
 import com.logistics.supply.model.RequestItem;
 import com.logistics.supply.model.Supplier;
@@ -29,7 +30,8 @@ public class ProcurementService extends AbstractDataService {
       RequestItem requestItem = requestItemService.findById(item.getId()).get();
       if (Objects.nonNull(requestItem)) {
         requestItem.setUnitPrice(procurementDTO.getUnitPrice());
-        var amount = procurementDTO.getUnitPrice().multiply(BigDecimal.valueOf(requestItem.getQuantity()));
+        var amount =
+            procurementDTO.getUnitPrice().multiply(BigDecimal.valueOf(requestItem.getQuantity()));
         System.out.println("amount: " + amount);
         requestItem.setTotalPrice(amount);
         Optional<Supplier> supplier =
@@ -49,7 +51,8 @@ public class ProcurementService extends AbstractDataService {
   }
 
   @Transactional(rollbackFor = Exception.class)
-  public RequestItem assignMultipleSuppliers(RequestItem item, Set<Supplier> multipleSuppliers, RequestCategory requestCategory) {
+  public RequestItem assignMultipleSuppliers(
+      RequestItem item, Set<Supplier> multipleSuppliers, RequestCategory requestCategory) {
     System.out.println("item = " + item.toString());
     if (item.getEndorsement().equals(EndorsementStatus.ENDORSED)
         && item.getStatus().equals(RequestStatus.PENDING)) {
@@ -58,6 +61,17 @@ public class ProcurementService extends AbstractDataService {
               .filter(s -> supplierRepository.existsById(Objects.requireNonNull(s.getId())))
               .map(x -> supplierRepository.findById(x.getId()).get())
               .collect(Collectors.toSet());
+      Set<Quotation> quotations =
+          suppliers.stream()
+              .map(
+                  x -> {
+                    Quotation q = new Quotation();
+                    q.setSupplier(x);
+                    Quotation result = quotationRepository.save(q);
+                    return result;
+                  })
+              .collect(Collectors.toSet());
+      item.setQuotations(quotations);
 
       return requestItemService.assignSuppliersToRequestItem(item, suppliers, requestCategory);
     }
