43af8284f42fa7be61297fbd5ef9c64657c4e8cb
Derrick <derrickagyemang12@gmail.com>
2022-06-23 20:04:37 +0000

    Remove redundant commented code

--- src/main/java/com/logistics/supply/dto/converter/CommentConverter.java
+++ src/main/java/com/logistics/supply/dto/converter/CommentConverter.java
@@ -1,6 +1,7 @@
 package com.logistics.supply.dto.converter;
 
 import com.logistics.supply.dto.CommentResponse;
+import com.logistics.supply.dto.RequestItemDTO;
 import com.logistics.supply.interfaces.GenericConverter;
 import com.logistics.supply.model.RequestItemComment;
 import org.springframework.beans.BeanUtils;
@@ -8,27 +9,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CommentConverter
-    implements GenericConverter<RequestItemComment, CommentResponse<RequestItemComment>> {
+    implements GenericConverter<RequestItemComment, CommentResponse<RequestItemDTO>> {
 
   @Override
-  public CommentResponse<RequestItemComment> apply(RequestItemComment requestItemComment) {
-    CommentResponse<RequestItemComment> commentResponse = new CommentResponse<>();
+  public CommentResponse<RequestItemDTO> apply(RequestItemComment requestItemComment) {
+    CommentResponse<RequestItemDTO> commentResponse = new CommentResponse<>();
     BeanUtils.copyProperties(requestItemComment, commentResponse);
-    commentResponse.setItem(requestItemComment);
+    RequestItemDTO requestItemDTO = RequestItemDTO.toDto(requestItemComment.getRequestItem());
+    commentResponse.setItem(requestItemDTO);
     return commentResponse;
   }
-
-
-  //  public static <T> Collection<CommentResponse<T>> toDto(Class<T> tClass, Collection<T> comment)
-  // {
-  //    List<CommentResponse<T>> tCollection = new ArrayList();
-  //    comment.forEach(
-  //        c -> {
-  //          CommentResponse<T> commentResponse = new CommentResponse();
-  //          BeanUtils.copyProperties(c, commentResponse);
-  //          tCollection.add(commentResponse);
-  //        });
-  //    return tCollection;
-  //  }
 
 }
