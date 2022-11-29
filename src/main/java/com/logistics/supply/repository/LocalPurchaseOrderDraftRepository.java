4d25346ea05c9ca392548f9ecde9a0870dcac741
Derrick <derrickagyemang12@gmail.com>
2022-11-29 16:42:45 +0200

    Remove date from query

--- src/main/java/com/logistics/supply/repository/LocalPurchaseOrderDraftRepository.java
+++ src/main/java/com/logistics/supply/repository/LocalPurchaseOrderDraftRepository.java
@@ -16,16 +16,9 @@ public interface LocalPurchaseOrderDraftRepository
 
   @Query(
       value =
-          "SELECT * from local_purchase_order_draft lpod where lpod.id not in (select lpo.local_purchase_order_draft_id from local_purchase_order lpo) and lpod.delivery_date >= current_date",
+          "SELECT * from local_purchase_order_draft lpod where lpod.id not in (select lpo.local_purchase_order_draft_id from local_purchase_order lpo)",
       nativeQuery = true)
   List<LocalPurchaseOrderDraft> findDraftAwaitingApproval();
-
-//  @Query(
-//      value =
-//          "SELECT * from local_purchase_order_draft lpod where lpod.id in "
-//              + "(SELECT grn.local_purchase_order_draft_id from goods_received_note grn) order by lpod.id DESC",
-//      nativeQuery = true)
-//  List<LocalPurchaseOrderDraft> findLPOLinkedToGRN();
 
   @Query(
       value =
